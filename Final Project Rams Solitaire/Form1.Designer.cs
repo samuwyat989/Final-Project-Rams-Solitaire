e3f451cda3f60d2af1feb4a52534d099f8c38b3e
samuwyat989 <samuwyat989@ed.amdsb.ca>
2017-01-12 13:33:31 -0500

    Converted to List Boxes

--- Final Project Rams Solitaire/Form1.Designer.cs
+++ Final Project Rams Solitaire/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.moveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -40,17 +40,24 @@
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.minLabel = new System.Windows.Forms.Label();
             this.secLabel = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.listBox7 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // moveButton
+            // loadButton
             // 
-            this.moveButton.Location = new System.Drawing.Point(28, 41);
-            this.moveButton.Name = "moveButton";
-            this.moveButton.Size = new System.Drawing.Size(75, 23);
-            this.moveButton.TabIndex = 0;
-            this.moveButton.Text = "MOVE";
-            this.moveButton.UseVisualStyleBackColor = true;
-            this.moveButton.Click += new System.EventHandler(this.moveButton_Click);
+            this.loadButton.Location = new System.Drawing.Point(28, 41);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "LOAD";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
             // 
             // textBox1
             // 
@@ -129,11 +136,75 @@
             this.secLabel.Size = new System.Drawing.Size(0, 13);
             this.secLabel.TabIndex = 9;
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(28, 248);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(100, 95);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(134, 248);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(100, 95);
+            this.listBox2.TabIndex = 11;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(240, 248);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(100, 95);
+            this.listBox3.TabIndex = 12;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(346, 248);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(100, 95);
+            this.listBox4.TabIndex = 13;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(452, 248);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(100, 95);
+            this.listBox5.TabIndex = 14;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(558, 248);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(100, 95);
+            this.listBox6.TabIndex = 15;
+            // 
+            // listBox7
+            // 
+            this.listBox7.FormattingEnabled = true;
+            this.listBox7.Location = new System.Drawing.Point(664, 248);
+            this.listBox7.Name = "listBox7";
+            this.listBox7.Size = new System.Drawing.Size(100, 95);
+            this.listBox7.TabIndex = 16;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(780, 484);
+            this.Controls.Add(this.listBox7);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.secLabel);
             this.Controls.Add(this.minLabel);
             this.Controls.Add(this.textBox7);
@@ -143,7 +214,7 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.moveButton);
+            this.Controls.Add(this.loadButton);
             this.Name = "Form1";
             this.Text = "Rams Solitaire";
             this.ResumeLayout(false);
@@ -153,7 +224,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button moveButton;
+        private System.Windows.Forms.Button loadButton;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
@@ -164,6 +235,13 @@
         private System.Windows.Forms.Timer gameTimer;
         private System.Windows.Forms.Label minLabel;
         private System.Windows.Forms.Label secLabel;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.ListBox listBox7;
     }
 }
 
--- Final Project Rams Solitaire/Form1.cs
+++ Final Project Rams Solitaire/Form1.cs
@@ -25,6 +25,8 @@ namespace Final_Project_Rams_Solitaire
 
         Random cardDeal = new Random();
 
+        List<string> startPile = new List<string>();
+
         List<string> drawPile = new List<string>();
         List<string> playPile = new List<string>();
 
@@ -47,8 +49,61 @@ namespace Final_Project_Rams_Solitaire
             gameTimer.Start();
         }
 
-        private void moveButton_Click(object sender, EventArgs e)
+        private void loadButton_Click(object sender, EventArgs e)
         {
+            startPile.Add("HA");
+            startPile.Add("DA");
+            startPile.Add("SA");
+            startPile.Add("CA");
+            startPile.Add("H2");
+            startPile.Add("D2");
+            startPile.Add("S2");
+            startPile.Add("C2");
+            startPile.Add("H3");
+            startPile.Add("D3");
+            startPile.Add("S3");
+            startPile.Add("C3");
+            startPile.Add("H4");
+            startPile.Add("D4");
+            startPile.Add("S4");
+            startPile.Add("C4");
+            startPile.Add("H5");
+            startPile.Add("D5");
+            startPile.Add("S5");
+            startPile.Add("C5");
+            startPile.Add("H6");
+            startPile.Add("D6");
+            startPile.Add("S6");
+            startPile.Add("C6");
+            startPile.Add("H7");
+            startPile.Add("D7");
+            startPile.Add("S7");
+            startPile.Add("C7");
+            startPile.Add("H8");
+            startPile.Add("D8");
+            startPile.Add("S8");
+            startPile.Add("C8");
+            startPile.Add("H9");
+            startPile.Add("D9");
+            startPile.Add("S9");
+            startPile.Add("C9");
+            startPile.Add("H10");
+            startPile.Add("D10");
+            startPile.Add("S10");
+            startPile.Add("C10");
+            startPile.Add("HJ");
+            startPile.Add("DJ");
+            startPile.Add("SJ");
+            startPile.Add("CJ");
+            startPile.Add("HQ");
+            startPile.Add("DQ");
+            startPile.Add("SQ");
+            startPile.Add("CQ");
+            startPile.Add("HK");
+            startPile.Add("DK");
+            startPile.Add("SK");
+            startPile.Add("CK");
+
             lineOne.Add("KingH");
             lineOne.Add("QueenC");
             lineTwo.Add("KingS");
@@ -59,9 +114,10 @@ namespace Final_Project_Rams_Solitaire
             textBox2.Text = lineTwo[0];
             textBox3.Text = lineThree[0];
 
-            cardUp = true;
-            suit = 2;
-            MoveCard(suit);
+            listBox1.Items.Add(lineOne[0] + Environment.NewLine);
+            listBox1.Items.Add(lineOne[1]);
+            listBox2.Items.Add(lineTwo[0]);
+            listBox3.Items.Add(lineThree[0]);
         }
 
         public void MoveCard(int suit)
@@ -96,9 +152,12 @@ namespace Final_Project_Rams_Solitaire
 
             if(lineTwo[0] == "KingH" || lineTwo[0] == "KingD")
             {
-                textBox1.Text = lineOne[0];
-                textBox2.Text = lineTwo[0] + Environment.NewLine;
-                textBox2.Text += lineOne[1];
+                //textBox1.Text = lineOne[0];
+                //textBox2.Text = lineTwo[0] + Environment.NewLine;
+                //textBox2.Text += lineOne[1];
+                listBox1.Items.Add(lineOne[0]);
+                listBox2.Items.Add(lineTwo[0] + Environment.NewLine);
+                listBox2.Items.Add(lineOne[1]);
                 lineTwo.Add(lineOne[1]);
                 lineOne.Remove(lineOne[1]);
             }
@@ -108,13 +167,16 @@ namespace Final_Project_Rams_Solitaire
 
             if (lineThree[0] == "KingH" || lineThree[0] == "KingD")
             {
-                textBox1.Text = lineOne[0];
-                textBox3.Text = lineThree[0] + Environment.NewLine;
-                textBox3.Text += lineOne[1];
+                //textBox1.Text = lineOne[0];
+                //textBox3.Text = lineThree[0] + Environment.NewLine;
+                //textBox3.Text += lineOne[1];
+                listBox1.Items.Add(lineOne[0]);
+                listBox3.Items.Add(lineThree[0] + Environment.NewLine);
+                listBox3.Items.Add(lineOne[1]);
                 lineThree.Add(lineOne[1]);
                 lineOne.Remove(lineOne[1]);
 
-                textBox3.Location = Point point{ 400,200];
+                //textBox3.Location = Point point{ 400,200];
             }
             else
             {
@@ -134,6 +196,13 @@ namespace Final_Project_Rams_Solitaire
 
             secLabel.Text = playTimeSec.ToString(":00");
             minLabel.Text = playTimeMin + "";
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            cardUp = true;
+            suit = 2;
+            MoveCard(suit);
         }
     }
 }
